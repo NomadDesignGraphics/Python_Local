4e106e54568b0d7d4e69aecc497b9e7b90d10780
NOMAD <gkhn_tsn_9@hotmail.com>
2020-10-14 10:28:43 +0300

    Create Python01.py
    Basit bilinmesi gerekenler

---
+++ Python01.py
@@ -0,0 +1,17 @@
+###Bu bölgede "Kick Start" deyimine adım atacaz
+
+#String ifade için [" --- "] tırnak içinde veri girilmelidir
+#örn.
+a = "Gökhan"
+
+#Not değişken isimler asla sayıyla başlamaz ve noktalama işaretiyle başlamaz
+
+#int değer için verilecek veri normal yazılır tırnak içine alınmasına gerek duyulmaz
+#örn.
+a1 = 18
+
+#Yazdırmak için [print("Veri" yada değişken adı)]şeklinde yazılır
+#örn.
+
+print("Kahvaltı Hazır!")
+print(a)
