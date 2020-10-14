aff38f05f0256c39e2bb4e1014e5dc4878e652d1
NOMAD <gkhn_tsn_9@hotmail.com>
2020-10-14 12:03:11 +0300

    Update Python02.py

--- Python02.py
+++ Python02.py
@@ -1,1 +1,21 @@
+###Kütüphane liste şeklindeki veri olayı
+#örn.
+#int_list = [1,2,3,4,5]
+
+names = [nomad,vergil,acepaladin]
+
+#bu yukarda belirtilen names artık ufak bir veri kütüphanesidir ve ordan veri çağırılabilir ama unutmayın veriler aslında 0 dan başlayıpta ilerler
+#misal names = [nomad = 0,vergil = 1, acepaladin = 2] şeklinde görüyor sistem aslında
+
+print(names[0]) #ekrana "nomad" yazdırır
+
+###EKLE/ÇIKAR/BELİRLİ YERE EKLE
+#kütüphaneye veri eklemek için
+names.append("Gökhan") #buraya "Gökhan" verisini ekletir
+
+#Kütüphaneden veri silmek için
+names.remove("Gökhan")
+
+#Belirli yere veri ekleme/belirtilmiş veriyi yeni girilenle değiştir
+names.insert(1, "Gökhan")
 
